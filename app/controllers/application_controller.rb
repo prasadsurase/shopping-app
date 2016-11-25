321ce7ca28bb4965c585ebcd00e4477ebe68db95
Prasad Surase <prasadsurase@gmail.com>
2016-11-25 17:05:05 +0530

    Added way to save order id in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_basket
+
+  private
+
+  def current_basket
+    session[:order_id] ? Order.find(session[:order_id]) : Order.new
+  end
 end
